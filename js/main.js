06c72df16a7c58454dc487eb8e429db04b54118f
Александр Теплов <74897344+AleksanderTeplov@users.noreply.github.com>
2021-02-13 15:57:28 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -162,6 +162,16 @@ $(document).ready(function () {
 })
 
 
+// Фиксированная шапка ======================================
+$(document).ready(function() {
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 0) {
+			$('.header').addClass('fixed')
+		} else {
+			$('.header').removeClass('fixed')
+		}
+	})
+})
 
 
 
@@ -179,17 +189,35 @@ $(function(){
 });
 
 
-// Фиксированная шапка ======================================
-$(document).ready(function() {
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > 0) {
-			$('.header').addClass('fixed')
-		} else {
-			$('.header').removeClass('fixed')
-		}
-	})
-})
 
+// Активный блок меню ======================================
+var positions = [], //сюда сложим на загрузке страницы позиции наших "якорных" блоков, чтобы не считать их каждый раз. и сюда же положим ссылки на соответствующие a.scroll-to
+    currentActive = null, //здесь будет храниться id текущего блока, чтобы не менять классы по 100 раз за одну прокрутку 
+    links = $('.menu__link'); //сохраним массив всех ссылок
 
+$(".anchor").each(function(){ //перебираем блоки, сохраняем позиции и ссылки на пункты меню
+    positions.push({
+        top: $(this).position().top - 100,
+        a: links.filter('[href="#'+$(this).attr('id')+'"]')
+    });
+});
+
+//делаем реверс массива, чтобы при скролле перебирать его с конца и выходить из цикла при нахождении
+//зачем нам проверять каждый блок, если прокрутка уже ниже последнего, верно?
+positions = positions.reverse(); 
+
+$(window).on('scroll',function() {
+    var winTop = $(window).scrollTop();
+    for(var i = 0; i < positions.length; i++){
+        if(positions[i].top < winTop){ //если прокрутка страницы ниже нашего блока
+            if(currentActive !== i){ //и если мы еще не добавили класс текущему блоку
+                currentActive = i;
+                links.filter('.active').removeClass('active'); //снимаем класс .active с текущего пункта меню
+                positions[i].a.addClass("active");
+            }
+            break; //выходим из цикла, не нужно проверять блоки, которые выше
+        }
+    }
+});
 
 
